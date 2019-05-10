6aff7384c6d6924b56e249cc79f8191db5e80168
mike_neck <jkrt3333@gmail.com>
2019-05-10 23:37:56 +0900

    add operation for ArrayList

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -16,9 +16,52 @@
 package com.example;
 
 import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.openjdk.jmh.runner.options.TimeValue;
+
+import java.util.Collection;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
+import java.util.stream.IntStream;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println(Runner.class.getCanonicalName());
+    public static void main(String[] args) throws RunnerException {
+        Options options = new OptionsBuilder()
+                .forks(4)
+                .jvmArgs("-Xmx256M", "-Xms256M", "-Dfile.encoding=UTF-8")
+                .shouldDoGC(true)
+                .measurementTime(TimeValue.seconds(5L))
+                .measurementIterations(4)
+                .syncIterations(true)
+                .forks(4)
+                .timeUnit(TimeUnit.MILLISECONDS)
+                .timeout(TimeValue.minutes(1L))
+                .warmupTime(TimeValue.seconds(4L))
+                .warmupIterations(4)
+                .build();
+        Runner runner = new Runner(options);
+        runner.list();
+        runner.run();
+    }
+
+    public static String randomString() {
+        ThreadLocalRandom random = ThreadLocalRandom.current();
+        StringBuilder builder = new StringBuilder();
+        IntStream.generate(() -> random.nextInt(26))
+                .limit(random.nextInt(512) + 1)
+                .map(ch -> (int) 'A' + ch)
+                .forEach(ch -> builder.append((char) ch));
+        return builder.toString();
+    }
+
+    public static <C extends Collection<String>> C randomCollection(int size, Supplier<? extends C> collectionSupplier) {
+        C collection = collectionSupplier.get();
+        while (collection.size() < size) {
+            collection.add(randomString());
+        }
+        return collection;
     }
 }
---
+++ src/main/java/com/example/list/ListBenchmark.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2019 Shinya Mochida
+ *
+ * Licensed under the Apache License,Version2.0(the"License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,software
+ * Distributed under the License is distributed on an"AS IS"BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.list;
+
+import com.example.Main;
+import org.openjdk.jmh.annotations.*;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+@State(Scope.Group)
+public abstract class ListBenchmark {
+
+    private final int size;
+    private final Supplier<? extends List<String>> supplier;
+    private final Consumer<? super List<String>> operation;
+    private final Consumer<? super List<String>> afterHook;
+
+    protected ListBenchmark(int size, Supplier<? extends List<String>> supplier, Consumer<? super List<String>> operation, Consumer<? super List<String>> afterHook) {
+        this.size = size;
+        this.supplier = supplier;
+        this.operation = operation;
+        this.afterHook = afterHook;
+    }
+
+    private List<String> list;
+
+    @Setup(Level.Trial)
+    @Group
+    public void setup() {
+        list = Main.randomCollection(size, supplier);
+    }
+
+    @Group
+    @Benchmark
+    @BenchmarkMode(Mode.Throughput)
+    public void benchmark() {
+        operation.accept(list);
+    }
+
+    @Group
+    @TearDown(Level.Invocation)
+    public void teardown() {
+        afterHook.accept(list);
+    }
+}
