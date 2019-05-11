974a632cb16d2ef079a59a46d9dc7b9121896e46
mike_neck <jkrt3333@gmail.com>
2019-05-12 08:41:50 +0900

    use tee to redirect to stdout and file.

--- app.sh
+++ app.sh
@@ -5,6 +5,6 @@ unzip jmh-java-collection-benchmark.zip
 
 artifact=jmh-java-collection-benchmark-`date "+%Y%m%d%H%M%S"`.txt
 
-jmh-java-collection-benchmark/bin/jmh-java-collection-benchmark > ${artifact}
+jmh-java-collection-benchmark/bin/jmh-java-collection-benchmark | tee ${artifact}
 
 aws s3api put-object --bucket org.mikeneck.test --key jmh/${artifact} --body ${artifact}
