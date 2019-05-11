236239f16b5d2ff7a6f987027612468aeb1f7498
mike_neck <jkrt3333@gmail.com>
2019-05-11 11:46:18 +0900

    complete app.sh

--- app.sh
+++ app.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
 aws s3api get-object --bucket org.mikeneck.test --key jmh-java-collection-benchmark.zip
+unzip jmh-java-collection-benchmark.zip
+
+artifact=jmh-java-collection-benchmark-`date "+%Y%m%d%H%M%S"`.txt
+
+jmh-java-collection-benchmark/bin/jmh-java-collection-benchmark > ${artifact}
+
+aws s3api put-object --bucket org.mikeneck.test --key jmh/${artifact} --body ${artifact}
