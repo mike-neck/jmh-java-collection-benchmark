8786b6a1015d2494cc44ea62cd85a8c2d27eea02
mike_neck <jkrt3333@gmail.com>
2019-05-19 01:20:46 +0900

    name duplicating.

--- buildSrc/src/main/groovy/com/example/ListOperation.groovy
+++ buildSrc/src/main/groovy/com/example/ListOperation.groovy
@@ -23,7 +23,7 @@ enum ListOperation {
     ADD_LAST('AddLast', ListOpString.ADD, ListOpString.REMOVE_LAST),
     ADD_FIRST('AddFirst', ListOpString.ADD_FIRST, ListOpString.REMOVE_LAST),
     ADD_MID('AddMid', ListOpString.ADD_MID, ListOpString.REMOVE_LAST),
-    REMOVE_FIRST('RemoveLast', ListOpString.REMOVE_FIRST, ListOpString.ADD),
+    REMOVE_FIRST('RemoveFirst', ListOpString.REMOVE_FIRST, ListOpString.ADD),
     REMOVE_LAST('RemoveLast', ListOpString.REMOVE_LAST, ListOpString.ADD),
     REMOVE_MID('RemoveMid', ListOpString.REMOVE_MID, ListOpString.ADD),
     ITERATION('Iteration', ListOpString.ITERATION, ListOpString.NO_OP),
